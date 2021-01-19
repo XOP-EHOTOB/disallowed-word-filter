86200c4c7a7389eab6d770247debe35b9fb0783b
XOP-EHOTOB <dima.dimitrev@gmail.com>
2021-01-19 17:18:11 +0300

    1.0

---
+++ index.js
@@ -0,0 +1,47 @@
+const disallowedWords = require('./disallowedWords.json')
+
+module.exports = class replaceDisallowedWords {
+  constructor(props) {
+    if (props && typeof props.additionalWords === 'string') {
+      this.additionalWords = props.additionalWords
+        .replace(/\s+/g, '')
+        .split(',')
+    } else {
+      this.additionalWords = null
+    }
+  }
+
+  check = (string, all) => {
+    if (!string | (typeof string != 'string'))
+      return console.error('Отсуствует строка для проверки!')
+
+    if (!all) {
+      if (!this.additionalWords)
+        return console.error('Нет списка запрещенных слов!')
+      return (
+        string
+          .toLowerCase()
+          .match(new RegExp(this.additionalWords.join('|'))) !== null
+      )
+    } else {
+      let allWords = [...this.additionalWords, ...disallowedWords]
+      return string.toLowerCase().match(new RegExp(allWords.join('|'))) !== null
+    }
+  }
+
+  replace = (string, change, all) => {
+    if (!string | (typeof string != 'string'))
+      return console.error('Отсуствует строка для проверки!')
+    if (!all) {
+      if (!this.additionalWords)
+        return console.error('Нет списка запрещенных слов!')
+      return string.replace(
+        new RegExp(this.additionalWords.join('|'), 'gi'),
+        change
+      )
+    } else {
+      let allWords = [...this.additionalWords, ...disallowedWords]
+      return string.replace(new RegExp(allWords.join('|'), 'gi'), change)
+    }
+  }
+}
